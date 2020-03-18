3c7c80f1ffaec9ee8b6f9ff58827d68a4f46b5ee
ileanazavoianu <ileana.zavoianu@yahoo.com>
2020-03-18 22:13:42 +0200

    Commit 1.Adaugare clasa TestGit

---
+++ TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.tema2;
+
+public class TestGit {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello Git ! Denumirea proiectului de licență este Comercilaizare online a produselor de ingrijire a sanatatii");
+	}
+
+}
